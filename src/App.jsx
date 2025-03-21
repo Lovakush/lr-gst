04d9324efcc87fae22a7a6f79d953411836a8b3c
Lovakush <20051303@kiit.ac.in>
2025-03-21 18:37:08 +0530

    update page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx (Router Version)
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import JustificationPage from './components/JustificationPage';
 
